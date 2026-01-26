58a4d1ffe38cacf0b87a3dcb4891d9464f8e31a1
Rynzier <rynziez@gmail.com>
2026-01-26 14:06:14 -0500

    Augh

--- SaltAndSulfur/BlockEntity/BlockEntityAlchemyFurnace.cs
+++ SaltAndSulfur/BlockEntity/BlockEntityAlchemyFurnace.cs
@@ -9,32 +9,11 @@ using Vintagestory.GameContent;
 
 namespace SaltAndSulfur
 {
-    public class BlockEntityAlchemyFurnace : BlockEntityOpenableContainer, IInteractable
+    public class BlockEntityAlchemyFurnace : BlockEntity, IInteractable
     {
-        protected InventoryAlchemyFurnace inventory;
-
-
-        public string DialogTitle => Block?.GetPlacedBlockName(Api?.World, Pos);
-        public override InventoryBase Inventory => inventory;
-        public override string InventoryClassName => "saltandsulfur:inventoryalchemyfurnace";
-
-        public BlockEntityAlchemyFurnace() : base()
-        {
-            inventory = new InventoryAlchemyFurnace(null, null);
-        }
-
-
-        public override bool OnPlayerRightClick(IPlayer byPlayer, BlockSelection blockSel)
-        {
-            return true;
-        }
-
         public bool OnBlockInteractStart(IWorldAccessor world, IPlayer byPlayer, BlockSelection blockSel, ref EnumHandling handling)
         {
-            if (world.Api.Side == EnumAppSide.Client)
-            {
-                byPlayer.Entity.Die();
-            }
+            Console.WriteLine("Test!");
             return true;
         }
     }
