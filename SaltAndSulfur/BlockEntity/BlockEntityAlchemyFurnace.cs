1575a73217d4b39b75d74332e73cfc5cc6f2adcd
Rynzier <rynziez@gmail.com>
2026-01-31 12:44:53 -0500

    More Furnace Experiments

--- SaltAndSulfur/BlockEntity/BlockEntityAlchemyFurnace.cs
+++ SaltAndSulfur/BlockEntity/BlockEntityAlchemyFurnace.cs
@@ -10,17 +10,31 @@ using Vintagestory.GameContent;
 
 namespace SaltAndSulfur
 {
-    public class BlockEntityAlchemyFurnace : BlockEntity, IInteractable
+    public class BlockEntityAlchemyFurnace : BlockEntityOpenableContainer
     {
         protected InventoryAlchemyFurnace inventory;
         protected GuiDialogAlchemyFurnace clientDialog;
 
         public string DialogTitle => Block?.GetPlacedBlockName(Api?.World, Pos);
-        public InventoryBase Inventory => inventory;
+        public override InventoryBase Inventory
+        {
+            get { return inventory; }
+        }
+
+        public override string InventoryClassName
+        {
+            get { return "alchemyfurnace"; }
+        }
 
         public BlockEntityAlchemyFurnace()
         {
             inventory = new InventoryAlchemyFurnace(null, null);
+            inventory.SlotModified += OnSlotModifid;
+        }
+
+        private void OnSlotModifid(int slotid)
+        {
+            return;
         }
 
         public override void Initialize(ICoreAPI api)
@@ -29,6 +43,21 @@ namespace SaltAndSulfur
             inventory.LateInitialize("alchemyfurnace-" + Pos.X + "/" + Pos.Y + "/" + Pos.Z, api);
         }
 
+        public override bool OnPlayerRightClick(IPlayer byPlayer, BlockSelection blockSel)
+        {
+            if (Api.Side == EnumAppSide.Client)
+            {
+                toggleInventoryDialogClient(byPlayer, () =>
+                {
+                    clientDialog = new GuiDialogAlchemyFurnace(DialogTitle, Inventory, Pos, Api as ICoreClientAPI);
+                    return clientDialog;
+                });
+            }
+            return true;
+        }
+
+
+        /*
         public bool OnBlockInteractStart(IWorldAccessor world, IPlayer byPlayer, BlockSelection blockSel, ref EnumHandling handling)
         {
             if (world.Api is ICoreClientAPI capi)
@@ -38,6 +67,26 @@ namespace SaltAndSulfur
             }
             return true;
         }
+        */
+
+        public override void OnReceivedClientPacket(IPlayer player, int packetid, byte[] data)
+        {
+            base.OnReceivedClientPacket(player, packetid, data);
+        }
+
+        public override void OnReceivedServerPacket(int packetid, byte[] data)
+        {
+            base.OnReceivedServerPacket(packetid, data);
+
+            if (packetid == (int)EnumBlockEntityPacketId.Close)
+            {
+                (Api.World as IClientWorldAccessor).Player.InventoryManager.CloseInventory(Inventory);
+                invDialog?.TryClose();
+                invDialog?.Dispose();
+                invDialog = null;
+            }
+        }
+
 
         public override void FromTreeAttributes(ITreeAttribute tree, IWorldAccessor worldAccessForResolve)
         {
